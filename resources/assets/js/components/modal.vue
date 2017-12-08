9debc0241b11e49d78bdcdf58fb8210101e94a98
Nick Basile <nicholas.basile@outlook.com>
2017-12-07 18:36:42 -0600

    Set Up Modal Component

---
+++ resources/assets/js/components/modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <h1>Header</h1>
+        </div>
+        <div>
+            <p>Body</p>
+        </div>
+        <div>
+            <button>Ok</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+
+          }
+        }
+    }
+</script>
