38283b231398480c21ac4664b6ef8976dcf47422
Nick Basile <nicholas.basile@outlook.com>
2017-12-07 21:01:46 -0600

    Added Modal Toggler

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,15 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="app" class="h-screen flex items-center justify-center bg-teal-lightest font-sans">
+<div id="app" class="h-screen flex flex-col items-center justify-center bg-teal-lightest font-sans">
+    <h1 class="text-center mb-8">Try Out These Modals</h1>
+    <div class="flex flex-col lg:flex-row lg:justify-between lg:w-3/4">
+        <button class="flex-no-shrink text-white py-2 px-4 mb-2 lg:mb-0 rounded bg-teal hover:bg-teal-dark" onclick="vueModal().show()">Default Modal</button>
+        <button class="flex-no-shrink text-white py-2 px-4 mb-2 lg:mb-0 rounded bg-green hover:bg-green-dark" onclick="vueModal().success().show()">Success Modal</button>
+        <button class="flex-no-shrink text-white py-2 px-4 mb-2 lg:mb-0 rounded bg-red hover:bg-red-dark" onclick="vueModal().error().show()">Error Modal</button>
+        <button class="flex-no-shrink text-white py-2 px-4 mb-2 lg:mb-0 rounded bg-blue hover:bg-blue-dark" onclick="vueModal().info().show()">Info Modal</button>
+        <button class="flex-no-shrink text-white py-2 px-4 mb-2 lg:mb-0 rounded bg-yellow hover:bg-yellow-dark" onclick="vueModal().warning().show()">Warning Modal</button>
+    </div>
     <modal-component></modal-component>
 </div>
 
